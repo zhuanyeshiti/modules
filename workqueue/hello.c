2a186e491fea18e717df71beb4590200dd5bf51a
zhuanyeshiti <liuhongwei123456@gmail.com>
2017-01-24 21:45:14 +0800

    modify

--- workqueue/hello.c
+++ workqueue/hello.c
@@ -5,10 +5,19 @@
 
 //for system_wq
 static struct work_struct work;
+static struct work_struct work1;
 
 static void work_handler(struct work_struct *data)
 {
-	pr_info("got into %s\n", __func__);
+	//pr_info("got into %s\n", __func__);
+	for(int i=0;i<3000;i++)
+		printk("1: %d\n",i);
+}
+
+static void work_handler1(struct work_struct *data)
+{
+	for(int i=3001;i<6000;i++)
+		printk("2: %d\n",i);
 }
 
 //for my workqueue
@@ -28,15 +37,16 @@ static int __init hello_init(void)
 {
     printk(KERN_INFO "Hello\n");
 	INIT_WORK(&work, work_handler);
-	
+	INIT_WORK(&work1,work_handler1);
 	// In kernel, there is a default workqueue list, named system_wq.
-	schedule_work(&work);
+	//schedule_work(&work);
 	
 	//my work queue, in USB system, there is a workqueue for USB transaction.
 	init_my_workqueue();
 	
 	//queue work on my work queue
 	queue_work(my_queue, &work);
+	queue_work(my_queue, &work1);
 
     return 0;
 }
